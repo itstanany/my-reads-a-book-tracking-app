2cc1fad159240cc5238b5a9aba1ec6a28c1047dd
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-07-11 09:11:12 +0200

    search feature - retrieving and displaying statis results

--- src/Components/SearchComponent/index.js
+++ src/Components/SearchComponent/index.js
@@ -1,4 +1,39 @@
+import { useCallback, useState } from 'react';
+import { search, update } from '../../BooksAPI';
+import Book from '../BookComponent';
+
 const Search = () => {
+  // const [query, setQuery] = useState('');
+  const [result, setResult] = useState([]);
+
+
+  /**
+   * get() of each search result and replace it to contain "SHELF" property
+   *    ~= 20 Ajax calls after initial search query
+   * getAll() and compare with search results
+   *    ~= n of shelves' books "filtering process"
+   */
+
+  // const find = useCallback(async (query) => {
+  //   const result = await search(query);
+  //   setResult(result);
+  // }, []);
+
+  const onUserInput = useCallback(async (e) => {
+    if (e.target.value) {
+      const result = await search(e.target.value);
+      // alert(JSON.stringify(result));
+      setResult(result.error ? [] : result);
+    } else {
+      setResult([]);
+    }
+  }, []);
+
+  const onShelfChange = useCallback(async (e, book) => {
+    await update(book, e.target.value);
+    // await updateBookCollections();
+  }, [])
+
   return (
     <div className="search-books">
       <div className="search-books-bar">
@@ -12,12 +47,25 @@ const Search = () => {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-          <input type="text" placeholder="Search by title or author" />
+          <input
+            type="text"
+            placeholder="Search by title or author"
+            onChange={onUserInput}
+          />
 
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+          {
+            result
+            && result.map((book) => {
+              console.log(book);
+              console.log(book.imageLinks);
+              return <Book book={book} onShelfChange={onShelfChange} />
+            })
+          }
+        </ol>
       </div>
     </div>)
 }
