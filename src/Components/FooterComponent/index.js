2e059d01e8e0e1edaaf8a94b58a5560772e50348
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-07-11 12:00:00 +0200

    cetralizing footer component

--- src/Components/FooterComponent/index.js
+++ src/Components/FooterComponent/index.js
@@ -1,14 +1,16 @@
 const Footer = () => {
   return (
-    <small>
-      Full Source Code of This Project Con be found at {' '}
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://github.com/ahmedalima/my-reads-a-book-tracking-app">
-        Here
-      </a>
-    </small>
+    <div class="footer">
+      <small>
+        Full Source Code of This Project Con be found at {' '}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/ahmedalima/my-reads-a-book-tracking-app">
+          Here
+        </a>
+      </small>
+    </div>
   );
 }
 
